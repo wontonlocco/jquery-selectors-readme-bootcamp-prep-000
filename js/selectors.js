2e0a01c792feadb2fac526191857c2c9ed78981a
Ed Wright <ew_wrightiii@live.com>
2018-01-04 13:32:04 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  let pTags = document.querySelector('p');
+  return pTags;
+}
+
+function lastImageSelector() {
+  return $('img').last();
+}
+
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+  return $('#pic-list li:first-child');
+}
