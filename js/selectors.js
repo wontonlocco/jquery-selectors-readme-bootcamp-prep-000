725fc4f2970aa3ab2d1ea69e938c89a8dda00c16
wontonlocco <brezzylocc@hotmail.com>
2018-01-04 08:28:42 -0500

    Update selectors.js

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,16 @@
 // declare your functions here...
-paragraphSelector(){
+function paragraphSelector(){
   let pTags = document.querySelector('p');
   return pTags;
+}
+
+function lastImageSelector() {
+  return $('img').last();
+}
+
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+  return $('#pic-list li:first-child');
 }
